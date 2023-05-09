651f25c3c2da69191117d1b0b835c33d42cb6f43
sharmanitincoder <sharmanitin.coder@gmail.com>
2023-05-09 22:26:38 +0530

    Initial Commit

---
+++ precache-manifest.718d4ba3f1e124ff80709a45aac1d3a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2eb0407729d6104c250",
+    "url": "/static/css/main.8b391cba.chunk.css"
+  },
+  {
+    "revision": "b2eb0407729d6104c250",
+    "url": "/static/js/main.b2eb0407.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "a750f122cd39658f696f",
+    "url": "/static/js/2.a750f122.chunk.js"
+  },
+  {
+    "revision": "72cba3a7707ecab0fd5b",
+    "url": "/static/js/runtime~main.72cba3a7.js"
+  },
+  {
+    "revision": "a797d4ba6dbc83cfdc2651f712da9dd8",
+    "url": "/index.html"
+  }
+];
